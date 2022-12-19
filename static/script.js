0bf4658f2360f88b8d0e0cdab95353eb175454cc
JacksonCakes <jacksonkek257@gmail.com>
2022-12-20 01:06:51 +0800

    UI update

--- static/script.js
+++ static/script.js
@@ -3,8 +3,6 @@ canvas = document.querySelector("canvas"),
 negPrompt = document.querySelector("textarea"),
 prompt = document.querySelector(".prompt"),
 toolBtns = document.querySelectorAll(".tool"),
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
 stepValue = document.querySelector("#step-value"),
 guideValue = document.querySelector("#guide-value"),
 stepsSlider = document.querySelector("#steps-slider"),
@@ -13,14 +11,11 @@ sizeSlider = document.querySelector("#size-slider"),
 rotateOptions = document.querySelectorAll(".rotate button"),
 previewImg = document.querySelector(".preview-img img"),
 clearCanvas = document.querySelector(".clear-canvas"),
-resetFilterBtn = document.querySelector(".reset-filter"),
 chooseImgBtn = document.querySelector(".choose-img"),
 saveImgBtn = document.querySelector(".save-img"),
 generateBtn = document.querySelector(".center-con"),
 ctx = canvas.getContext("2d",{ willReadFrequently: true });
 
-let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
-let rotate = 0, flipHorizontal = 1, flipVertical = 1;
 // global variables with default value
 let prevMouseX, prevMouseY, snapshot,
 isDrawing = false,
@@ -36,7 +31,6 @@ const loadImage = () => {
     if(!file) return;
     previewImg.src = URL.createObjectURL(file);
     previewImg.addEventListener("load", () => {
-        resetFilterBtn.click();
         document.querySelector(".container").classList.remove("disable");
         canvas.width = previewImg.clientWidth;
         canvas.height = previewImg.clientHeight;
@@ -83,28 +77,6 @@ const drawing = (e) => {
     }
 }
 sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value); // passing slider value as brushSize
-const applyFilter = () => {
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
-    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-}
-
-toolBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all tool option
-        // removing active class from the previous option and adding on current clicked option
-        document.querySelector(".options-brush .active").classList.remove("active");
-        btn.classList.add("active");
-        selectedTool = btn.id;
-    });
-});
-
-filterOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        document.querySelector(".active").classList.remove("active");
-        option.classList.add("active");
-   
-
-    });
-});
 
 const updateFilter = () => {
     stepValue.innerText = `${stepsSlider.value}`;
@@ -126,7 +98,7 @@ rotateOptions.forEach(option => {
             // Push an element to
             // the top of stack
             Redo.push(latest_edit);
-        } else if(option.id === "right") {
+        } else {
             // Stores the top element
             // of the stack
             let cur_edit = Redo[Redo.length - 1];
@@ -136,29 +108,15 @@ rotateOptions.forEach(option => {
             // Push an element to
             // the top of the stack
             Undo.push(cur_edit);
-        } else if(option.id === "horizontal") {
-            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
-        } else {
-            flipVertical = flipVertical === 1 ? -1 : 1;
-        }
+        } 
         //applyFilter();
     });
 });
 
-const resetFilter = () => {
-    brightness = "100"; saturation = "100"; inversion = "0"; grayscale = "0";
-    rotate = 0; flipHorizontal = 1; flipVertical = 1;
-    filterOptions[0].click();
-    applyFilter();
-}
 
-clearCanvas.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing whole canvas
-});
 
 const saveImage = () => {
     const link = document.createElement("a"); // creating <a> element
-    const img = document.createElement("img");
     const img_canvas = document.createElement("canvas");
     img_canvas.width = previewImg.clientWidth;
     img_canvas.height = previewImg.clientHeight;
@@ -221,7 +179,7 @@ canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", () => isDrawing = false);
 stepsSlider.addEventListener("input", updateFilter);
 guideSlider.addEventListener("input", updateGuide);
-resetFilterBtn.addEventListener("click", resetFilter);
+
 saveImgBtn.addEventListener("click", saveImage);
 generateBtn.addEventListener("click", sendData);
 fileInput.addEventListener("change", loadImage);
